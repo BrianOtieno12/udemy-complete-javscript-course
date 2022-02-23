5f790706494e416855ffe5fbfe18794b0af21696
unknown <noriamy52@gmail.com>
2022-02-23 06:24:42 +0300

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,38 @@
 let js = 'amazing';
-if (js === 'amazing') {
-    alert('JavaScript is FUN');
-}
-
 console.log(40 + 8 + 23 - 10);
+
+/**
+ * Value is the smallest unit of information that we have in js
+ * examples of values are jonas which is a string
+ * and 23 which is an integer
+ */
+console.log("BrianOtieno12");
+console.log(23);
+
+/**
+ * Variables are temporary storage locations of values in a 
+ * computer memory that enable you to re-use them 
+ * eg variable firstName stores value Brian
+ */
+
+//Variable declaration
+let firstName = "Brian";
+console.log(firstName);
+
+/**
+ * Variable naming conventions
+ * 1. use camelCase notation
+ * 2. variables names should not start with a number
+ * 3, variable names can only contain numbers, letters, underscores or dollarsigns
+ * 4. reserved keywords cannot be used as a variable name
+ * 5. avoid using variable called name
+ * 6. variable names should not start with an uppercase letter(it os a convention not rule)
+ * 7. ensure a variable name is descriptive
+ */
+let myFirstJob = "Coder";
+let myCurrentJob = "teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
