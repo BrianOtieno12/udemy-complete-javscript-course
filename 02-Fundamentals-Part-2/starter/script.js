5c4cbc0e7240db8e9ed3fd2113d8162584a32bc8
unknown <noriamy52@gmail.com>
2022-04-18 08:24:28 +0300

    Introduction to functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,17 +1,31 @@
 //enable strict mode
-/**
- * if you disable strict mode by commenting line 5...
- * ...the error on line 13 will not be shown and js will fail silently.
- * Strict mode is also used to throw an error when you use a variable name...
- * ..that js has reserved for future use
- */
 'use strict';
 
-let hasDriversLicense = false;
-const passed = true;
+// /**
+//  * Strict mode
+//  * if you disable strict mode by commenting line 5...
+//  * ...the error on line 13 will not be shown and js will fail silently.
+//  * Strict mode is also used to throw an error when you use a variable name...
+//  * ..that js has reserved for future use
+//  */
+// let hasDriversLicense = false;
+// const passed = true;
 
-if (passed) hasDriverLicense = true;
-if (hasDriversLicense) console.log('I can drive 🚗');
+// if (passed) hasDriverLicense = true;
+// if (hasDriversLicense) console.log('I can drive 🚗');
 
-//future use reserved key word example
-const interface = 'hello world';
+// //future use reserved key word example
+// const interface = 'hello world';
+
+/**
+ * Functions: these are reusable codes grouped together that can be....
+ * ..called many times in the program
+ */
+
+/**
+ * function declaration
+ * we use key word function followed by function name plus brackets
+ */
+function logger() {
+    console.log('Hello world');
+}
